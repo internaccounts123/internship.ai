0f66576179c3ed2444926e5c12b3f7114c13369e
Rashid Nizamani <rashid.nizamani@hotmail.com>
2019-01-08 17:43:22 +0500

    changed both the generator and main file to account for individual examples instead of files

--- generator.py
+++ generator.py
@@ -1,24 +1,33 @@
 import pickle
 
-def create_batch(file_paths,batch_size,batch_index):
+def create_batch(file_paths,batch_size,file_index,starting_index):
     batch=[]
-    for i in range(batch_size):
-            scaling=batch_index*batch_size+i
-            with open(file_paths[scaling], 'rb') as f:
-                batch.append(list(pickle.load(f)))
-    return batch        
+    loop=True
+    while loop:
+        with open(file_paths[file_index], 'rb') as f:
+            file=(list(pickle.load(f))) 
+            if len(file[starting_index:])+len(batch)<batch_size:
+                batch.extend(file[starting_index:])
+                file_index+=1
+                starting_index=0
+            else:
+                appending_examples=batch_size-len(batch)
+                batch.extend(file[starting_index:starting_index+appending_examples])
+                starting_index+=appending_examples
+                loop=False
+                
+    return batch,file_index,starting_index        
 
-def data_generator(file_paths,batch_size=1):
+def data_generator(file_paths,batch_size):
     #file_paths contains the absolute paths of all the files we need to convert and save
     #batch_size tells us how many files to load and save in a single go(depends upon the size of RAM)
     
-    num_batches=len(file_paths)/batch_size
-    assert (num_batches==int(num_batches))
-    num_batches=int(num_batches)
+    file_index=0
+    starting_index=0
     
-    for i in range(num_batches):
-        batch=create_batch(file_paths,batch_size,i)   
+    while True:
+        batch,file_index,starting_index=create_batch(file_paths,batch_size,file_index,starting_index)   
         yield batch
-
-
-
+        if file_index>=len(file_paths):
+            break 
+        
--- main.py
+++ main.py
@@ -1,21 +1,29 @@
 import os
 import sys
 
-def main(save_type='csv'):
+def main(directory= os.path.dirname(os.path.realpath(__file__)),save_type='csv',batch_size=100000):
+
+    #convert our arguments into integers 
+    batch_size=int(batch_size)
+    #get file path
     dir_path = os.path.dirname(os.path.realpath(__file__))
     #get paths of all the pkl files 
     paths=get_pkl(directory = dir_path, max_depth=-1)
     #make generator that contains all the batches 
-    generator=data_generator(paths,1)
+    batches=data_generator(paths,batch_size)
+    file_name=1
     for batch in batches:
-        #convert each batch 
-        dataframe=conv_to_pd_dataframe(batch)
-        #save each batch
-        save(dataframe,save_type)
-    print (dir_path,save_type)
-  
+            #convert each batch 
+            dataframe=conv_to_pd_dataframe(batch)
+            save(dataframe,str(file_name)+'.'+save_type,directory)
+            file_name+=1
+          
 if __name__=="__main__":
-    if len(sys.argv)>1:
+    if len(sys.argv)==2:
         main(sys.argv[1])
+    elif len(sys.argv)==3:
+        main(sys.argv[1],sys.argv[2])
+    elif len(sys.argv)==4:
+        main(sys.argv[1],sys.argv[2],sys.argv[3])
     else:
         main()
