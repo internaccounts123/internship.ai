6be64fd3181fa9c512af563f3321372da380b5d0
Muhammad Usman <muhammadusman1.me@gmail.com>
2019-01-17 16:15:43 +0500

    Predict added

---
+++ predict/DeepNN.py
@@ -0,0 +1,48 @@
+from keras.layers import Dense
+from keras.layers import BatchNormalization
+from keras.layers import Input
+from keras.models import Model
+import numpy as np
+
+class DNN:
+    """
+    Class to construct Fully Connected Deep Networks
+    """
+    def __init__(self, config):
+        """
+
+        :param config: Dictionary of network configurations
+        """
+        self.input_shape = config['Input_shape']
+        self.layer_shapes = config['layer_shapes']
+        self.activation = config['Activation']
+        self.output = config['Output']
+        self.weights=config['Weights']
+        self.model=self.construct_fc_model()
+        if self.weights:
+            self.load_weights()
+
+    def construct_fc_model(self):
+        """
+        Constructs a Fully Connected model
+        :return: Model object of keras
+        """
+        input_layer = Input(shape=(self.input_shape,))
+        layer = input_layer
+        for layer_neurons in self.layer_shapes:
+            layer = Dense(layer_neurons, activation=self.activation)(layer)
+        layer = Dense(self.output)(layer)
+        return Model(input_layer, layer)
+
+    def load_weights(self):
+        """
+        Load weights into a model
+        :return: Nothing
+        """
+        self.model.load_weights(self.weights)
+
+    def predict(self, input):
+        """
+        Returns the predicted label
+        """
+        return self.model.predict(np.atleast_2d(input))
---
+++ predict/Predict.py
@@ -0,0 +1,38 @@
+from DeepNN import DNN
+import numpy as np
+from keras import optimizers
+import keras.backend as KB
+import keras.losses as Loss
+import tensorflow as tf
+import os
+import sys
+import pickle
+class Network:
+    def __init__(self,path):
+        self.path=path
+        self.config= {'layer_shapes': [300, 300, 300, 100, 50], 'Activation': 'relu',
+             'Output': 6, 'Input_shape': 472,
+                      'Weights':os.path.join(self.path,'Weights.h5')}
+        self.dnn=DNN(self.config)
+    def predict(self,example):
+        
+        
+        mean = np.load(os.path.join(self.path,'mean.npy'))
+        std = np.load(os.path.join(self.path,'std.npy'))
+        if example[-2]=='Unknown':
+            example[-2]='Accelerate'
+
+        
+        with open(os.path.join(self.path,"lane_change_mode.pkl"), 'rb') as f:  
+            lane_encoder=pickle.load(f)
+        with open(os.path.join(self.path,"previous_decision.pkl"), 'rb') as f:  
+            decision_encoder=pickle.load(f)
+        with open(os.path.join(self.path,"action.pkl"), 'rb') as f:  
+            action_encoder=pickle.load(f)
+            
+        example[-2]=decision_encoder.transform([example[-2]])[0]
+        example[1]=lane_encoder.transform([example[1]])[0]
+        example=example.astype(float)
+        example=(example-mean)/std
+        action=np.argmax(np.array(self.dnn.predict(example)), axis=1)[0]
+        return action_encoder.inverse_transform([action])[0]
