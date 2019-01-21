bf27ff0b7b00c9f89ac9bf292a527adb2cec2f5e
Muhammad Usman <muhammadusman1.me@gmail.com>
2019-01-21 11:10:22 +0500

    Refactored keras_generator.py

--- data_utils/keras_generator.py
+++ data_utils/keras_generator.py
@@ -5,7 +5,14 @@ import glob
 import pandas as pd
 from multiprocessing import Queue
 class Generator(keras.utils.Sequence):
+    """
+    Multiprocessing implementation of Keras Generator to read Data and feed to network
+    """
     def __init__(self, config):
+        """
+
+        :param config:Network configurations
+        """
         self.config = config
         self.actions_count=self.config['labels']
         self.DataDirectory = self.config['data_directory']
@@ -20,12 +27,22 @@ class Generator(keras.utils.Sequence):
         self.i_f = 0
 
     def fill_buffer(self):
+        """
+        Put files into Multiprocessing Queue
+        :return:Nothing
+        """
         files_batch_size = self.config['file_batch_size']
         files=self.DataFiles[self.Indexes_files[self.i_f:self.i_f+files_batch_size]]
         self.Data.put(self.load_files(files, self.config['format_']))
         self.i_f += files_batch_size
 
     def load_files(self, files, format_):
+        """
+
+        :param files:List of files to load
+        :param format_:csv or h5
+        :return:nd array of data loaded
+        """
         data_list = []
         if format_ == 'csv':
             for i in files:
@@ -42,13 +59,12 @@ class Generator(keras.utils.Sequence):
                     self.obsnet_col=array.columns=='ob_net'
                     self.action_col=array.columns=='action'
                     data_list.extend(array.values)
-        elif format_ == 'npy':
-            for i in files:
-                if i[-4:] == '.csv':
-                    array = np.load(os.path.join(self.DataDirectory, i))
-                    data_list.extend(array)
         return np.array(data_list)
     def generate(self):
+        """
+        Fills Data into multiprocessing queue when there's space availabe in a queue
+        :return: Nothing
+        """
         while self.Run==True:
             if self.Data.qsize()>=self.config['max_queue_size']:
                 continue
@@ -57,6 +73,10 @@ class Generator(keras.utils.Sequence):
                 self.i_f = 0
             self.fill_buffer()
     def load_data(self):
+        """
+        Fetch data from multiprocessing queue
+        :return: nd array of data
+        """
         self.examples_batch_size = self.config['ex_batch_size']
         if self.i_e >= len(self.Data_selected):
             self.Data_selected=self.Data.get(block=True,timeout=None)
@@ -67,10 +87,19 @@ class Generator(keras.utils.Sequence):
         self.i_e += self.examples_batch_size
         return res
     def __len__(self):
+        """
+
+        :return:  Total mini batches in an epoch
+        """
         return (len(self.DataFiles)*self.config['file_examples'])//self.config['ex_batch_size']
     def on_epoch_end(self):
         pass
     def __getitem__(self, idx):
+        """
+
+        :param idx: idx of mini-batch(Not used but required by keras)
+        :return: (X,y) to network
+        """
         data = self.load_data()
         data1 = data[:,(~self.action_col)]
         action=data[:,self.action_col]
