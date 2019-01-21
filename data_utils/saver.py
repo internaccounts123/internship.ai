4cceaee6cc9b7f9bbb92f48fa5428c9138556be1
Muhammad Usman <muhammadusman1.me@gmail.com>
2019-01-21 10:33:38 +0500

    Refactored saver.py

--- data_utils/saver.py
+++ data_utils/saver.py
@@ -3,14 +3,28 @@ import numpy as np
 import pandas as pd
 import csv
 import json
+import os
 class Data_Saver:
+    """
+    Parallelised implementation of Data saving class
+    """
     def __init__(self,filename,save_type,out):
+        """
+        :param filename:  Name of the output file
+        :param save_type: h5,npy or csv
+        :param out: Abs Path of the output Directory
+        """
         self.current_rows=0
         self.file_name=filename
         self.save_type=save_type
         self.output_dir=out
+
     def save(self,Q):
-        import os
+        """
+        Saves data in a specified format
+        :param Q: Mutiprocessing Queue which serves a purpose of shared memory
+        :return: Nothing
+        """
         while True:
             print('Running_SAVE')
             self.filename=os.path.join(self.output_dir, str(self.file_name) + '.' + self.save_type)
@@ -28,11 +42,21 @@ class Data_Saver:
             self.file_name+=1
             print ('saved')
     def save_h5(self,dataframe):
+        """
+        Saves data to hard drive in h5 format
+        :param dataframe: Pandas data frame to save
+        :return: Nothing
+        """
         dataframe['key']=pd.Series(np.arange(0,dataframe.shape[0],dtype=int))
         print( self.filename)
         dataframe.to_hdf(self.filename,key='key')
         print ('saved   ')
     def save_npy(self,dataframe):
+        """
+        Saves data in npy format and its columns in json format
+        :param dataframe: Pandas data frame to save
+        :return: Nothing
+        """
         dataframe['key'] = pd.Series(np.arange(0, dataframe.shape[0], dtype=int))
         array=dataframe.values
         np.save(self.filename,array)
@@ -44,12 +68,10 @@ class Data_Saver:
 
     def save_csv(self, data, mode='w'):
         """
-        @Author: Salman Ahmed
-
-            data is a dataframe having rows in it to be appended or saved.
-            filename is name of file where data should be saved.
-            mode, if mode is w that means we are writing it first time
-            if mode is a then it will append in already created file.
+        Saves csv to hard drive
+        :param data: Pandas Data frame
+        :param mode: a: append, w: write
+        :return:Nothing
         """
         filename=self.filename
         dataframe['key'] = pd.Series(np.arange(0, dataframe.shape[0], dtype=int))
