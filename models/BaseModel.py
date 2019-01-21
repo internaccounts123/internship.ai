f16d774f8e531aa44ecd8bd564407e382f27ba22
Armughan-Shahid <marmughanshahid@gmail.com>
2019-01-21 13:12:28 +0500

    refactored code

--- models/BaseModel.py
+++ models/BaseModel.py
@@ -1,11 +1,9 @@
-import tensorflow as tf
-import keras.layers as L
-from utils.callbacks import get_tensorboard_callback,get_checkpoint_call_back
+
+from utils.callbacks import get_tensorboard_callback, get_checkpoint_call_back
 
 
-
-class Base_Model():
-    def __init__(self,input_shape,num_outputs,optimizer,prefix):
+class BaseModel:
+    def __init__(self, input_shape, num_outputs, optimizer, prefix):
         """
         params:
             input_shape: a list that specifies input_shapes 
@@ -16,51 +14,69 @@ class Base_Model():
                         
         """
         
-        self.input_shape=input_shape
-        self.num_outputs=num_outputs
-        self.optimizer=optimizer
-        self.prefix=prefix
+        self.input_shape = input_shape
+        self.num_outputs = num_outputs
+        self.optimizer = optimizer
+        self.prefix = prefix
+        self.Model = None
         
     def construct_model(self):
         """
         Model is constructed here and self.Model stores the link to the Model
         """
         raise Exception('Unimplemented Error ')
-    
-        
-    def train_generator(self,generator,epochs=10,logdir="../../logs",checkpoint_dir="../../checkpoints",save_N_epochs=10):
-        """
-        
-        """
-        tensorboard=get_tensorboard_callback(logdir=logdir+"/"+self.prefix)
-        checkpoint=get_checkpoint_call_back(checkpoint_dir,self.prefix+"_chkpoint_",period=save_N_epochs)
-        
-        self.Model.fit_generator(generator= generator, 
-                    steps_per_epoch  = len(generator), 
-                    epochs           = epochs, 
-                    verbose          = 1,
-                    max_queue_size   = 3,
-                                callbacks=[tensorboard,checkpoint])
 
-    def train(self,traindataX,traindataY,valdataX,valdataY,epochs=10,logdir="../../logs",
-              checkpoint_dir="../../checkpoints",save_N_epochs=10):
+    def train_generator(self, generator, epochs=10, log_dir="../../logs", checkpoint_dir="../../checkpoints",
+                        save_n_epochs=10):
         """
-        
+
+        :param generator: gives a list [inputs,targets] that is fed into the model
+        :param epochs: num of epochs to train the model
+        :param log_dir: directory where to place logs
+        :param checkpoint_dir: directory in which to save checkpoints
+        :param save_n_epochs: save model weights after N epochs
+        :return:
         """
-        tensorboard=get_tensorboard_callback(logdir=logdir+"/"+self.prefix)
-        checkpoint=get_checkpoint_call_back(checkpoint_dir,self.prefix+"_chkpoint_",period=save_N_epochs)
+        tensorboard = get_tensorboard_callback(logdir=log_dir+"/"+self.prefix)
+        checkpoint = get_checkpoint_call_back(checkpoint_dir,self.prefix+"_checkpoint_", period=save_n_epochs)
         
-        self.Model.fit(x=traindataX,
-                       y=traindataY,
-                       validation_data=(valdataX,valdataY),
-                    batch_size  = 64,
-                    epochs           = epochs, 
-                    verbose          = 1,
-                    callbacks=[tensorboard,checkpoint])
-        
-    def predict(self,observation):
+        self.Model.fit_generator(generator=generator,
+                                 steps_per_epoch=len(generator),
+                                 epochs=epochs,
+                                 verbose=1,
+                                 max_queue_size=3,
+                                 callbacks=[tensorboard, checkpoint])
+
+    def train(self, train_data_x, train_data_y, val_data_x, val_data_y, epochs=10, log_dir="../../logs",
+              checkpoint_dir="../../checkpoints", save_n_epochs=10):
         """
-        predicts output given an input
+
+        :param train_data_x: list of training inputs ,shape of each input=(numexamples,numfeatures)
+        :param train_data_y: list of training outputs
+        :param val_data_x: list of validation inputs ,shape of each input=(numexamples,numfeatures)
+        :param val_data_y: list of validations outputs
+        :param epochs:  num of epochs
+        :param log_dir:  directory where to place logs
+        :param checkpoint_dir: directory in which to save checkpoints
+        :param save_n_epochs: save model weights after N epochs
+        :return:
+        """
+        tensorboard = get_tensorboard_callback(logdir=log_dir + "/" + self.prefix)
+        checkpoint = get_checkpoint_call_back(checkpoint_dir, self.prefix+"_checkpoint_", period=save_n_epochs)
+        
+        self.Model.fit(x=train_data_x,
+                       y=train_data_y,
+                       validation_data=(val_data_x, val_data_y),
+                       batch_size=64,
+                       epochs=epochs,
+                       verbose=1,
+                       callbacks=[tensorboard,checkpoint])
+        
+    def predict(self, observation):
+        """
+
+        :param observation:
+        :return:
         """
         
         raise Exception('Unimplemented Error ')
--- utils/tools.py
+++ utils/tools.py
@@ -1,34 +1,34 @@
 import pandas as pd
-import keras.layers as L
+import keras.layers as layers
 
-def construct_model_from_csv(model_file,input_placeholder):
+
+def construct_model_from_csv(model_file, input_placeholder):
         """
-        @Author: Armughan
-        given a csv file this function constructs a graph from it
-        
-        model_file: path of csv file that contains details about the model
-        input placeholder: input tensor
+
+        :param model_file: path of csv file containing info about the design of the model
+        :param input_placeholder: this is used as input to the model
+        :return x: the last layer (tensor) of the model
         """
-        model_df=pd.read_csv(model_file).fillna(0)
-        model_df = model_df.astype({"layer_type": str, "units": int,"strides":int,
-                                   "padding":str,"kernel_size":int,"num_filters":int})
+        model_df = pd.read_csv(model_file).fillna(0)
+        model_df = model_df.astype({"layer_type": str, "units": int, "strides":int,
+                                   "padding": str, "kernel_size": int, "num_filters": int})
         x=input_placeholder
         for i in range(model_df.shape[0]):
             layer_type=model_df.layer_type[i]
-            assert layer_type in ["conv","conv1D","fc","flatten","relu","bn","pooling"],"invalid layer type"
+            assert layer_type in ["conv", "conv1D", "fc", "flatten", "relu", "bn", "pooling"], "invalid layer type"
             
-            if layer_type=="conv":
+            if layer_type == "conv":
                 padding=str(model_df.padding[i])
-                kernel_size=int(model_df.kernel_size[i])
-                num_filters=int(model_df.num_filters[i])
-                strides=int(model_df.strides[i])
+                kernel_size = int(model_df.kernel_size[i])
+                num_filters = int(model_df.num_filters[i])
+                strides = int(model_df.strides[i])
                 
-                assert padding=="same" or padding=="valid"
-                assert kernel_size>0
-                assert num_filters>0
-                assert strides>0
-                
-                x=L.Conv2D(num_filters,kernel_size=kernel_size,strides=strides,padding=padding)(x)
+                assert padding == "same" or padding == "valid"
+                assert kernel_size > 0
+                assert num_filters > 0
+                assert strides > 0
+
+                x = layers.Conv2D(num_filters, kernel_size=kernel_size, strides=strides, padding=padding)(x)
 
             if layer_type == "conv1D":
                 padding = str(model_df.padding[i])
@@ -41,30 +41,30 @@ def construct_model_from_csv(model_file,input_placeholder):
                 assert num_filters > 0
                 assert strides > 0
 
-                x = L.Conv1D(num_filters, kernel_size=kernel_size, strides=strides, padding=padding)(x)
+                x = layers.Conv1D(num_filters, kernel_size=kernel_size, strides=strides, padding=padding)(x)
                 
-            elif layer_type=="fc":
-                hidden_units=int(model_df.units[i])
-                assert hidden_units>0
-                x=L.Dense(hidden_units)(x)
+            elif layer_type == "fc":
+                hidden_units = int(model_df.units[i])
+                assert hidden_units > 0
+                x = layers.Dense(hidden_units)(x)
                 
-            elif layer_type=="flatten":
-                x=L.Flatten()(x)
+            elif layer_type == "flatten":
+                x = layers.Flatten()(x)
                 
-            elif layer_type=="relu":
-                x=L.Activation("relu")(x)
+            elif layer_type == "relu":
+                x = layers.Activation("relu")(x)
                 
-            elif layer_type=="bn":
-                x=L.BatchNormalization()(x)
+            elif layer_type == "bn":
+                x = layers.BatchNormalization()(x)
                 
-            elif layer_type=="pooling":
-                padding=int(model_df.padding[i])
-                kernel_size=int(model_df.kernel_size[i])
-                strides=int(model_df.strides[i])
+            elif layer_type == "pooling":
+                padding = int(model_df.padding[i])
+                kernel_size = int(model_df.kernel_size[i])
+                strides = int(model_df.strides[i])
                 
-                assert padding=="same" or padding=="valid"
-                assert kernel_size>0
-                assert strides>0
-                x=L.MaxPooling2D(kernel_size,strides=strides,padding=padding)(x)
+                assert padding == "same" or padding == "valid"
+                assert kernel_size > 0
+                assert strides > 0
+                x = layers.MaxPooling2D(kernel_size, strides=strides, padding=padding)(x)
                 
         return x
